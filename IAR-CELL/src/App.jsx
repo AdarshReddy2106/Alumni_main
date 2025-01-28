0749f001282579dc3212f361ebb4d4af1ccaf625
Awake-ctrl <kallepallysaikira21@gmail.com>
2025-01-29 00:45:05 +0530

    about page added

--- IAR-CELL/src/App.jsx
+++ IAR-CELL/src/App.jsx
@@ -1,16 +1,10 @@
-// ?import React from 'react'
-<<<<<<< HEAD
+import React from 'react'
 // import { useState } from 'react';
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop"
 import About from "./pages/About"
-=======
-import { useState } from 'react';
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
-import Home from "./pages/Home"
+
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop"
->>>>>>> 1f4d2fee108ee5162d929b18a98ba32efc7b5ec0
 
 function App() {
 
@@ -22,12 +16,9 @@ function App() {
       
       <Routes>
         
-        <Route path="/" element={<Home/>}/>
-<<<<<<< HEAD
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-
-=======
->>>>>>> 1f4d2fee108ee5162d929b18a98ba32efc7b5ec0
+        
        
         </Routes>
       </Router>
