6793337a3de45699fe39bb57254b98cf22a0c7f7
Awake-ctrl <kallepallysaikiran21@gmail.com>
2025-03-13 01:22:03 +0530

    Update iar_cell_year_2024.sql

--- iar_cell_year_2024.sql
+++ iar_cell_year_2024.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `year_2024`
 --
-
+USE iar_cell;
 DROP TABLE IF EXISTS `year_2024`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
